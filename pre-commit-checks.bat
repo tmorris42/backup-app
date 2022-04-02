c30e1aac6f37e560ba0c92a5504abce9be48a50c
taylorhmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-04-02 13:17:28 +0200

    Update pre-commit-checks

--- pre-commit-checks.bat
+++ pre-commit-checks.bat
@@ -1,12 +1,12 @@
 ECHO OFF
 python -m isort -q --skip .\env\
-python -m black -q --line-length 79 backup_app.py
-python -m black -q --line-length 79 tests.py
+python -m black -q --line-length 79 backup_app
+python -m black -q --line-length 79 tests
 python -m mypy --warn-unused-ignores .
-python -m flake8 backup_app.py --ignore E231,E203
-python -m flake8 tests.py
-python -m pylint backup_app.py
-python -m pylint tests.py
-python -m pytest -q tests.py
-python -m bandit backup_app.py -q
+python -m flake8 backup_app --ignore E231,E203
+python -m flake8 tests
+python -m pylint backup_app
+python -m pylint tests
+python -m pytest -q tests\tests.py
+python -m bandit -r -q backup_app
 ECHO ON
