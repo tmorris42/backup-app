f1d39491de2b9900633d3adebf62c80611e929bc
taylorhmorris <18409399+taylorhmorris@users.noreply.github.com>
2022-04-06 00:23:34 +0200

    Update backup_manager to remove changed entries from report
    - backup_app now automatically redisplays the report periodically

--- backup_app/backup_manager.py
+++ backup_app/backup_manager.py
@@ -29,7 +29,7 @@ class BackupManager:
             )
         else:
             self.log_file = None
-        self.report = Report({})
+        self.report = Report()
 
     def log(self, msg, pretty=False):
         """Log messages to the session log file."""
@@ -75,6 +75,14 @@ class BackupManager:
             filenames,
             overwrite=overwrite,
         )
+
+        failed_set = set(failed)
+        copied_set = set(filenames) - failed_set
+        for file in copied_set:
+            if file in self.report["moved_files"]:
+                self.report["moved_files"].remove(file)
+            if file in self.report["mismatched_files"]:
+                self.report["mismatched_files"].remove(file)
         return failed
 
     def copy_added_file(self, filename):
@@ -102,24 +110,42 @@ class BackupManager:
 
         Return filenames that were not updated.
         """
-        return update_files_a_to_b(
+        failed = update_files_a_to_b(
             self.source_directory,
             self.backup_directory,
             self.report["mismatched_files"],
         )
+        self.report["mismatched_files"] = failed
+        return failed
 
-    def delete_files(self):
+    def delete_files(self, files_to_delete=None):
         """Delete files that were removed from the source directory.
 
         Return filenames that were not deleted.
         """
-        return delete_files_from_b(
-            self.backup_directory, self.report["removed_files"]
+        if files_to_delete is None:
+            files_to_delete = self.report["removed_files"]
+        failed = delete_files_from_b(
+            self.backup_directory, files_to_delete
         )
 
-    def move_files(self):
+        failed_set = set(failed)
+        deleted_set = set(files_to_delete) - failed_set
+        for file in deleted_set:
+            self.report["removed_files"].remove(file)
+        return failed
+
+    def move_files(self, files_to_move=None):
         """Move files that were moved in the source directory.
 
         Return filenames that were not moved.
         """
-        return move_files_in_b(self.report["moved_files"])
+        if files_to_move is None:
+            files_to_move = self.report["moved_files"]
+        failed = move_files_in_b(files_to_move)
+
+        failed_set = set(failed)
+        moved_set = set(files_to_move) - failed_set
+        for file in moved_set:
+            self.report["moved_files"].remove(file)
+        return failed
